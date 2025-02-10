dddac54f8e90bf82f18b5ef8a32991846017f2d7
Randall Nagy <randall.nagy@gmail.com>
2025-02-10 06:56:53 -0500

    .

--- RssScheduler.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# RssScheduler.py: Name says it all?
-# Rev 0.00
-# Status: Marker.
-
-class Scheduler:
-    pass
--- __init__.py
+++
@@ -1,3 +0,0 @@
-from .Nexus import RSSSite
-
-__all__ = ['rss9000', 'RSSSite']
